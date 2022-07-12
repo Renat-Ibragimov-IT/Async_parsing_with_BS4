71dee935b9533dac97203936e99270bb6239db1a
renat-ibragimov-it <renatibragimov26@gmail.com>
2022-07-12 22:03:06 +0300

    Program updated

--- asyncio_parsing.py
+++ asyncio_parsing.py
@@ -1,20 +1,22 @@
 import asyncio
-import aiohttp
+from pprint import pp
 
+import aiohttp
 from bs4 import BeautifulSoup
 
-user_agent = {'user-agent': 'Mozilla/5.0'}
-
-all_data = []
+USER_AGENT = {'user-agent': 'Mozilla/5.0'}
 
 
 async def get_page_data(session, url):
     """Function to receive information from the website"""
-    async with session.get(url, headers=user_agent) as resp:
-        resp_text = await resp.text()
-        soup = BeautifulSoup(resp_text, "lxml")
-        title = soup.find('title')
-        all_data.append(f'{url} - {title.text}')
+    try:
+        async with session.get(url, headers=USER_AGENT, timeout=5) as resp:
+            resp_text = await resp.text()
+        return {'url': url, 'html': resp_text}
+    except aiohttp.ClientConnectorError:
+        print(f'{url}: Page not found')
+    except asyncio.exceptions.TimeoutError:
+        print(f'{url}: Timeout Error')
 
 
 async def load_site_data(urls):
@@ -22,16 +24,30 @@ async def load_site_data(urls):
     async with aiohttp.ClientSession() as session:
         tasks = []
         for url in urls:
-            try:
-                task = asyncio.create_task(get_page_data(session, url))
-                tasks.append(task)
-                await asyncio.gather(*tasks)
-            except aiohttp.ClientConnectorError:
-                pass
+            task = asyncio.create_task(get_page_data(session, url))
+            tasks.append(task)
+        return await asyncio.gather(*tasks)
+
+
+async def main(urls):
+    """This function will run program"""
+    async with aiohttp.ClientSession():
+        return await load_site_data(urls)
+
+
+def parser(data):
+    """This function will extract required part of info"""
+    all_data = []
+    for info in data:
+        if info:
+            soup = BeautifulSoup(info['html'], "html.parser")
+            title = soup.find('title')
+            all_data.append(f'{info["url"]} - {title.text}')
+    return pp(all_data)
 
 
 if __name__ == '__main__':
     with open("news_sites.txt") as file:
-        urls = [f'http://{website}' for website in file.read().split('\n')]
-    asyncio.run(load_site_data(urls))
-    print("\n".join(all_data))
+        links = [f'http://{website}' for website in file.read().split('\n')]
+    all_info = asyncio.run(main(links))
+    parser(all_info)
